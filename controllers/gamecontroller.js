496aaa8939042bee5edfb84f2808faad6794214d
Aleksey Kulgeyko <kulgeyko93@mail.ru>
2021-05-22 18:20:43 +0300

    fix mistakes and fix syntaxis

--- controllers/gamecontroller.js
+++ controllers/gamecontroller.js
@@ -1,116 +1,88 @@
-var router = require('express').Router();
-var Game = require('../models/game');
+const router = require('express').Router();
+const Game = require('../models/game');
 
-router.get('/all', (req, res) => {
-    Game.findAll({ where: { owner_id: req.user.id } })
-        .then(
-            function findSuccess(data) {
-                res.status(200).json({
-                    games: games,
-                    message: "Data fetched."
-                })
-            },
+router.get('/all', async (req, res) => {
+    try {
+        const games = await Game.findAll({ where: { owner_id: req.user.id } });
 
-            function findFail() {
-                res.status(500).json({
-                    message: "Data not found"
-                })
-            }
-        )
+        if (!games)  res.status(404).json("Not found");
+
+        res.status(200).json(`games: ${games}`);
+    } catch (e) {
+        res.status(500).json(e);
+    }
 })
 
 router.get('/:id', (req, res) => {
-    Game.findOne({ where: { id: req.params.id, owner_id: req.user.id } })
-        .then(
-            function findSuccess(game) {
-                res.status(200).json({
-                    game: game
-                })
-            },
+    try {
+        const game = await Game.findOne({ where: { id: req.params.id, owner_id: req.user.id } });
 
-            function findFail(err) {
-                res.status(500).json({
-                    message: "Data not found."
-                })
-            }
-        )
+        if (!game)  res.status(404).json("Not found");
+
+        res.status(200).json(`games: ${game}`);
+    } catch (e) {
+        res.status(500).json(e);
+    }
 })
 
 router.post('/create', (req, res) => {
-    Game.create({
-        title: req.body.game.title,
-        owner_id: req.body.user.id,
-        studio: req.body.game.studio,
-        esrb_rating: req.body.game.esrb_rating,
-        user_rating: req.body.game.user_rating,
-        have_played: req.body.game.have_played
-    })
-        .then(
-            function createSuccess(game) {
-                res.status(200).json({
-                    game: game,
-                    message: "Game created."
-                })
-            },
+    try {
+        const game = await Game.create({
+            title: req.body.game.title,
+            owner_id: req.body.user.id,
+            studio: req.body.game.studio,
+            esrb_rating: req.body.game.esrb_rating,
+            user_rating: req.body.game.user_rating,
+            have_played: req.body.game.have_played
+        });
 
-            function createFail(err) {
-                res.status(500).send(err.message)
-            }
-        )
+        if (!game)  res.status(404).json("Not found");
+
+        res.status(200).json(`games: ${game}, Game created.`);
+    } catch (e) {
+        res.status(500).json(e);
+    }
 })
 
 router.put('/update/:id', (req, res) => {
-    Game.update({
-        title: req.body.game.title,
-        studio: req.body.game.studio,
-        esrb_rating: req.body.game.esrb_rating,
-        user_rating: req.body.game.user_rating,
-        have_played: req.body.game.have_played
-    },
-        {
-            where: {
-                id: req.params.id,
-                owner_id: req.user
-            }
-        })
-        .then(
-            function updateSuccess(game) {
-                res.status(200).json({
-                    game: game,
-                    message: "Successfully updated."
-                })
-            },
+    try {
+        const gameUpdate = Game.update({
+            title: req.body.game.title,
+            studio: req.body.game.studio,
+            esrb_rating: req.body.game.esrb_rating,
+            user_rating: req.body.game.user_rating,
+            have_played: req.body.game.have_played
+        },
+            {
+                where: {
+                    id: req.params.id,
+                    owner_id: req.user
+                }
+            });
 
-            function updateFail(err) {
-                res.status(500).json({
-                    message: err.message
-                })
-            }
+        if (!gameUpdate)  res.status(404).json("Not found");
 
-        )
+        res.status(200).json(`games: ${gameUpdate}, Game created.`);
+    } catch (e) {
+        res.status(500).json(e);
+    }
 })
 
 router.delete('/remove/:id', (req, res) => {
-    Game.destroy({
-        where: {
-            id: req.params.id,
-            owner_id: req.user.id
-        }
-    })
-    .then(
-        function deleteSuccess(game) {
-            res.status(200).json({
-                game: game,
-                message: "Successfully deleted"
-            })
-        },
+    try {
+        const game = Game.destroy({
+            where: {
+                id: req.params.id,
+                owner_id: req.user.id
+            }
+        });
 
-        function deleteFail(err) {
-            res.status(500).json({
-                error: err.message
-            })
-        }
-    )
+        if (!game)  res.status(404).json("Not found");
+
+        res.status(200).json(`games: ${game}, Game created.`);
+    } catch (e) {
+        res.status(500).json(e);
+    }
 })
 
 module.exports = router;
--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,38 +1,43 @@
 const router = require('express').Router();
-var bcrypt = require('bcryptjs');
-var jwt = require('jsonwebtoken');
-var User = require('../models/user');
-
-// var User = require('../db').import('../models/user');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
 
 router.post('/signup', (req, res) => {
-    User.create({
-        full_name: req.body.user.full_name,
-        username: req.body.user.username,
-        passwordhash: bcrypt.hashSync(req.body.user.password, 10),
-        email: req.body.user.email,
-    })
-        .then(
-            function signupSuccess(user) {
-                let token = jwt.sign({ id: user.id }, 'lets_play_sum_games_man', { expiresIn: 60 * 60 * 24 });
-                res.status(200).json({
-                    user: user,
-                    token: token
-                })
-            },
+    try {
+        const body = req.body
 
-            function signupFail(err) {
-                res.status(500).send(err.message)
-            }
-        )
+        const user = await User.create({
+        full_name: body.full_name,
+        username: body.username,
+        passwordhash: bcrypt.hashSync(body.password, 10),
+        email: body.email,
+    });
+
+        if (!user) res.status(404).json("Not found");
+
+        let token = jwt.sign({ id: user.id }, 'lets_play_sum_games_man', { expiresIn: 60 * 60 * 24 });
+        res.status(200).json({
+            user: user,
+            token: token
+        });
+
+        res.status(200).json(`games: ${user}`);
+    } catch (e) {
+        res.status(500).json(e);
+    }
 })
 
 router.post('/signin', (req, res) => {
-    User.findOne({ where: { username: req.body.user.username } }).then(user => {
+    try {
+        const body = req.body
+
+        const user = await User.findOne({ where: { username: body.username } })
+
         if (user) {
-            bcrypt.compare(req.body.user.password, user.passwordHash, function (err, matches) {
+            bcrypt.compare(req.body.password, passwordHash, function (err, matches) {
                 if (matches) {
-                    var token = jwt.sign({ id: user.id }, 'lets_play_sum_games_man', { expiresIn: 60 * 60 * 24 });
+                    var token = jwt.sign({ id: id }, 'lets_play_sum_games_man', { expiresIn: 60 * 60 * 24 });
                     res.json({
                         user: user,
                         message: "Successfully authenticated.",
@@ -45,8 +50,9 @@ router.post('/signin', (req, res) => {
         } else {
             res.status(403).send({ error: "User not found." })
         }
-
-    })
+    } catch (e) {
+        res.status(500).json(e);
+    }
 })
 
 module.exports = router;
